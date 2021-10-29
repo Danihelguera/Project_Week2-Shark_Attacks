8874d4e3d8549f1814b8190e0001f2ac9cb5c710
Danihelguera <danielhelguerasanchez@gmail.com>
2021-10-29 16:09:49 +0200

    Inicio

---
+++ src/cleaning_functions.py
@@ -0,0 +1,17 @@
+import pandas as ps
+
+def suma(a,b)
+    """
+    Esta funcion suma los argumentos
+
+    Args:
+        a (int, float) - Es un numero a sumar
+
+        b (int, float) - Es un numero a sumar
+
+    Return: 
+        La suma de los argumentos
+    """
+
+    return a+b
+
